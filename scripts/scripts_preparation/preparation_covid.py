a511ca64599c38ed38180dcdc9c334ccd2831543
Bastien <bastien.robert91@gmail.com>
2020-11-19 11:35:34 +0100

    structure

---
+++ scripts/scripts_preparation/preparation_covid.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 19 10:48:09 2020
+
+@author: Bast
+"""
+
+# 1) Récuperer l'ensemble des fichiers dans departements
+# 2) Construction d'un dataset unique
+# 3) Ajout colonne Region
+# 4) Recherche coordonnées régions
+
+
+from scripts.scripts_preparation
---
+++ scripts/scripts_preparation/preparation_tweets.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 19 10:47:47 2020
+
+@author: Bast
+"""
+
+# 1) Recuperation des fichiers tweets
+# 2) Construction d'un unique dataset
+# 3) Extraction du texte avec une regex
+# 4) Recuperation des localisations
+
+
+def hello():
+    print('hello')
